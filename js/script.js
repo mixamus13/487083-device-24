c70840c059ebf8e89ceb755925e8b7c609e59d0c
Maksim Dokuchaev <mixamus@me.com>
2019-03-18 22:12:33 +0300

    popup write is done

--- js/script.js
+++ js/script.js
@@ -8,8 +8,6 @@ var map = document.querySelector(".modal-map");
 
 var closes = document.querySelector(".modal-close-map");
 
-var storage = localStorage.getItem("name");
-
 var form = popup.querySelector("form");
 var name = popup.querySelector("[name=name]");
 var email = popup.querySelector("[name=email]");
@@ -36,16 +34,11 @@ closes.addEventListener("click", function (evt) {
   popup.classList.remove("modal-error");
 });
 
-
 form.addEventListener("submit", function (evt) {
   if (!name.value || !email.value) {
     evt.preventDefault();
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("email", email.value);
-    }
   }
 });
