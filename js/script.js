d1aaaff8cacb2bb3d71ed1894428f84cb1665618
Maksim Dokuchaev <mixamus@me.com>
2019-03-18 09:19:04 +0300

    valide form is done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 var link = document.querySelector(".map-button-yellow");
+
 var popup = document.querySelector(".modal-write");
 var close = document.querySelector(".modal-close-write");
+
 var linz = document.querySelector(".office-device");
 var map = document.querySelector(".modal-map");
+
 var closes = document.querySelector(".modal-close-map");
+
+var storege = localStorage.getItem("name");
+
+var form = popup.querySelector("form");
+var name = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -18,9 +27,25 @@ linz.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 closes.addEventListener("click", function (evt) {
   evt.preventDefault();
   map.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+
+form.addEventListener("submit", function (evt) {
+  if (!name.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("email", email.value);
+    }
+  }
 });
