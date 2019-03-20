0d40e565340bee36e9a3f984de2fbffe6f33d965
Maksim Dokuchaev <mixamus@me.com>
2019-03-20 09:36:33 +0300

    Б15 1-5 is done

--- js/script.js
+++ js/script.js
@@ -1,44 +1,44 @@
-var link = document.querySelector(".map-button-yellow");
+var link = document.querySelector('.map-button-yellow');
 
-var popup = document.querySelector(".modal-write");
-var close = document.querySelector(".modal-close-write");
+var popup = document.querySelector('.modal-write');
+var close = document.querySelector('.modal-close-write');
 
-var linz = document.querySelector(".office-device");
-var map = document.querySelector(".modal-map");
+var linz = document.querySelector('.office-device');
+var map = document.querySelector('.modal-map');
 
-var closes = document.querySelector(".modal-close-map");
+var closes = document.querySelector('.modal-close-map');
 
-var form = popup.querySelector("form");
-var name = popup.querySelector("[name=name]");
-var email = popup.querySelector("[name=email]");
+var form = popup.querySelector('form');
+var name = popup.querySelector('[name=name]');
+var email = popup.querySelector('[name=email]');
 
-link.addEventListener("click", function (evt) {
+link.addEventListener('click', function (evt) {
   evt.preventDefault();
-  popup.classList.add("modal-show");
+  popup.classList.add('modal-show');
 });
 
-linz.addEventListener("click", function (evt) {
+linz.addEventListener('click', function (evt) {
   evt.preventDefault();
-  map.classList.add("modal-show");
+  map.classList.add('modal-show');
 });
 
-close.addEventListener("click", function (evt) {
+close.addEventListener('click', function (evt) {
   evt.preventDefault();
-  popup.classList.remove("modal-show");
-  popup.classList.remove("modal-error");
+  popup.classList.remove('modal-show');
+  popup.classList.remove('modal-error');
 });
 
-closes.addEventListener("click", function (evt) {
+closes.addEventListener('click', function (evt) {
   evt.preventDefault();
-  map.classList.remove("modal-show");
-  popup.classList.remove("modal-error");
+  map.classList.remove('modal-show');
+  popup.classList.remove('modal-error');
 });
 
-form.addEventListener("submit", function (evt) {
+form.addEventListener('submit', function (evt) {
   if (!name.value || !email.value) {
     evt.preventDefault();
-    popup.classList.remove("modal-error");
+    popup.classList.remove('modal-error');
     popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error");
+    popup.classList.add('modal-error');
   }
 });
